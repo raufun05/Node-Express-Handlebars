917eb53d33719f680709dd18355b69fe3dced444
Raufun <raufun05@yahoo.com>
2020-09-16 21:40:06 -0600

    added db file

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers_db.burgers (burger_name) VALUES ("veggie burger");
+INSERT INTO burgers_db.burgers (burger_name) VALUES ("Cheeseburger");
+INSERT INTO burgers_db.burgers (burger_name) VALUES ("hamburger");
