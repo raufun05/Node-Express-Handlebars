8d651b1ca371e9f68924cc4f0f9439041dadb0ec
Raufun <raufun05@yahoo.com>
2020-09-20 22:57:25 -0600

    updated controller file

--- config/connection.js
+++ config/connection.js
@@ -25,3 +25,24 @@ if (process.env.JAWSDB_URL){
 connection.connect();
 // Export connection for our ORM to use.
 module.exports = connection;
+
+
+// var mysql = require("mysql");
+
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "password",
+//   database: "burgers_db"
+// });
+
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+
+// module.exports = connection;
